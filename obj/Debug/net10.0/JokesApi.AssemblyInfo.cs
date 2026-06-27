429a0c4924421828a6ef60734bba28a7948cbde6
Gisella González <riptheraven@gmail.com>
2026-06-27 16:13:28 -0300

    first commit

--- obj/Debug/net10.0/JokesApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/JokesApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JokesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6026302aa8e9e6d60ebc6f373f5c1e8b9d05a0d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("JokesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JokesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
