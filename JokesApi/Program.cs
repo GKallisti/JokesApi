b87e782fd6f848abd533732652f409de4a199fb6
Gisella González <riptheraven@gmail.com>
2026-06-27 18:17:18 -0300

    move github actions to repo root

---
+++ JokesApi/Program.cs
@@ -0,0 +1,36 @@
+var  builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+var jokes = new List<Joke>
+{
+    new Joke { Id = 1, Text = "What do you call a fake noodle? An impasta." },
+    new Joke { Id = 2, Text = "Why did the scarecrow win an award? Because he was outstanding in his field!" },
+    new Joke { Id = 3, Text = "Why did the bicycle fall over? Because it was two-tired!" },
+    new Joke { Id = 4, Text = "Why did the math book look sad? Because it had too many problems." },
+    new Joke { Id = 5, Text = "Why did the tomato turn red? Because it saw the salad dressing!" }
+};
+
+app.MapGet("/jokes/random", () =>
+{
+    var random = new Random();
+    int index = random.Next(jokes.Count);
+    return jokes[index];
+});
+
+app.MapGet("/", () => "Welcome this cursed API! Use the /joke endpoint to get a random joke.");
+
+app.MapPost("/jokes", (Joke newJoke) =>
+{   newJoke.Id= jokes.Max(j => j.Id) + 1;
+    jokes.Add(newJoke);
+    return Results.Created($"/jokes/{newJoke.Id}", newJoke);
+
+});
+
+app.MapGet("/jokes", () => jokes);
+
+app.Run();
+
+public class Joke
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+}
